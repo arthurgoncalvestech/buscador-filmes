b4662ae1135ff87fb08a9aad0fee3e545b5a6bc7
Arthur Gonçalves Silva <arthur.goncalves.work@gmail.com>
2026-08-28 14:07:44 -0300

    adiciona estilização e detalhes extras do filme

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 const botao = document.getElementById("botao-busca");
 const campo = document.getElementById("campo-busca");
-const resultado = document.getElementById("resultado");    
-    
-    botao.addEventListener("click", async function () {
-    const filme = campo.value;
+const resultado = document.getElementById("resultado");
 
-    if (filme.trim() === "") {
-        resultado.innerHTML = `<p>Digite o nome de um filme.</p>`;
-        return;
-    }
+botao.addEventListener("click", async function () {
+  const filme = campo.value;
 
-    const url = `https://www.omdbapi.com/?apikey=5718388a&t=${filme}`;
+  if (filme.trim() === "") {
+    resultado.innerHTML = `<p>Digite o nome de um filme.</p>`;
+    return;
+  }
 
-    try {
-        const resposta = await fetch(url);
-        const dados = await resposta.json();
+  const url = `https://www.omdbapi.com/?apikey=5718388a&t=${filme}`;
 
-        console.log(dados);
+  try {
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
 
-        if (dados.Response === "True") {
-        resultado.innerHTML = `
-            <h2>${dados.Title}</h2>
-            <p>${dados.Plot}</p>
-        `;
-        } else {
-        resultado.innerHTML = `<p>Filme não encontrado.</p>`;
+    console.log(dados);
+
+    if (dados.Response === "True") {
+      resultado.innerHTML = `
+        ${dados.Poster !== "N/A"
+          ? `<img src="${dados.Poster}" alt="Pôster de ${dados.Title}">`
+          : ""
         }
-    } catch (erro) {
-        resultado.innerHTML = `<p>Erro ao buscar o filme. Tente novamente.</p>`;
+
+        <h2>${dados.Title} (${dados.Year})</h2>
+        <p>${dados.Plot}</p>
+        <p><strong>Gênero:</strong> ${dados.Genre}</p>
+        <p><strong>Diretor:</strong> ${dados.Director}</p>
+        <p><strong>Elenco:</strong> ${dados.Actors}</p>
+        <p><strong>Nota IMDb:</strong> ${dados.imdbRating}</p>
+      `;
+    } else {
+      resultado.innerHTML = `<p>Filme não encontrado.</p>`;
     }
-    });
+  } catch (erro) {
+    resultado.innerHTML = `<p>Erro ao buscar o filme. Tente novamente.</p>`;
+  }
+});
